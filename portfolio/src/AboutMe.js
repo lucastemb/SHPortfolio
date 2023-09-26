11086ecade94ecc8e6d2490a0b1da7fec1b25fec
Lucas Tembras <51347268+lucastemb@users.noreply.github.com>
2023-09-26 15:01:45 -0400

    added color to about me and finished navbar

--- portfolio/src/AboutMe.js
+++ portfolio/src/AboutMe.js
@@ -3,19 +3,21 @@ import './App.css';
 
 function AboutMe() {
   return (
-    <div className="flex-col">
-    <nav className="flex-row justify-center align-center">
-      <div className='text-left'>
-        <h2 className="font-sans font-semibold"> Lucas Tembras </h2> 
+    <div className="flex flex-col bg-[#72A9BE]/50 h-screen">
+    <nav className="flex flex-row justify-between bg-[#0070C0]/90 text-white">
+      <div className="">
+      <h2 className="font-sans text-xl font-semibold m-8"> Lucas Tembras </h2> 
       </div>
-      <div className='text-right'>
+      <div className='m-8'>
       <a className="font-sans m-8">About Me</a>
       <a className="font-sans m-8">Work Experience</a>
       <a className="font-sans m-8">Projects</a>
       <a className="font-sans m-8">Contact Me</a>
       </div>
     </nav>
-    <div className="about-me">
+    {/* Info Section */}
+    <div className="">
+      <img src=""></img>
       <h2 className="text-red"> About Me </h2>
     </div>
     </div>
