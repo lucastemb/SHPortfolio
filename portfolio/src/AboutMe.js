d01969e1d626cd5ab73142379620a5bcacb46df6
Lucas Tembras <51347268+lucastemb@users.noreply.github.com>
2023-09-26 02:44:54 -0400

    minor advancements with styling of navbar

--- portfolio/src/AboutMe.js
+++ portfolio/src/AboutMe.js
@@ -3,15 +3,20 @@ import './App.css';
 
 function AboutMe() {
   return (
-    <div className="flex-col justify-center"> 
-    <nav className="flex-col justify-between"> 
-      <a classname="font-sans text-sm">About Me</a>
-      <a classname="font-sans text-sm">Work Experience</a>
-      <a classname="font-sans text-sm">Projects</a>
-      <a classname="font-sans text-sm">Contact Me</a> 
+    <div className="flex-col">
+    <nav className="flex-row justify-center align-center">
+      <div className='text-left'>
+        <h2 className="font-sans font-semibold"> Lucas Tembras </h2> 
+      </div>
+      <div className='text-right'>
+      <a className="font-sans m-8">About Me</a>
+      <a className="font-sans m-8">Work Experience</a>
+      <a className="font-sans m-8">Projects</a>
+      <a className="font-sans m-8">Contact Me</a>
+      </div>
     </nav>
     <div className="about-me">
-      <h2> About Me </h2>
+      <h2 className="text-red"> About Me </h2>
     </div>
     </div>
   );
