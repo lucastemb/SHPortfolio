a2238f1779e4a1486ab70b01abcdc4fccbb1f8f7
Lucas Tembras <51347268+lucastemb@users.noreply.github.com>
2023-09-26 23:04:38 -0400

    more "about me" styling updates

--- portfolio/src/AboutMe.js
+++ portfolio/src/AboutMe.js
@@ -6,10 +6,10 @@ function AboutMe() {
   return (
     <div className="flex flex-col bg-[#72A9BE]/50 h-screen">
     <nav className="flex flex-row align-center justify-between text-[#001F5B] h-16">
-      <div className="m-5 flex flex-col justify-center">
+      <div className="m-8 flex flex-col justify-center">
       <h2 className="font-sans text-xl font-bold"> Lucas Tembras </h2> 
       </div>
-      <div className='m-5 text-lg font-semibold'>
+      <div className='m-8 text-lg font-semibold'>
       <a className="font-sans m-10">About Me</a>
       <a className="font-sans m-10">Experience</a>
       <a className="font-sans m-10">Projects</a>
@@ -17,8 +17,8 @@ function AboutMe() {
       </div>
     </nav>
     {/* Info Section */}
-    <div className="flex flex-row align-center justify-around h-full items-center">
-    <div className="flex flex-col text-white font-sans font-semibold"> 
+    <div className="flex flex-row align-center justify-center h-full items-center">
+    <div className="flex flex-col text-white font-sans font-semibold w-[35%]"> 
     <h2 className="text-white text-6xl text-[#001F5B]"> About Me </h2>
     <p className="font-normal text-[#001F5B]"> Hi, I'm Lucas Tembras. A 3rd-year undergraduate at the University of Florida studying Computer Science looking for Software Engineering roles. </p>
     <span className="flex flex-row fill-[#001F5B]">
@@ -26,8 +26,10 @@ function AboutMe() {
     <svg className="m-2" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-4.466 19.59c-.405.078-.534-.171-.534-.384v-2.195c0-.747-.262-1.233-.55-1.481 1.782-.198 3.654-.875 3.654-3.947 0-.874-.312-1.588-.823-2.147.082-.202.356-1.016-.079-2.117 0 0-.671-.215-2.198.82-.64-.18-1.324-.267-2.004-.271-.68.003-1.364.091-2.003.269-1.528-1.035-2.2-.82-2.2-.82-.434 1.102-.16 1.915-.077 2.118-.512.56-.824 1.273-.824 2.147 0 3.064 1.867 3.751 3.645 3.954-.229.2-.436.552-.508 1.07-.457.204-1.614.557-2.328-.666 0 0-.423-.768-1.227-.825 0 0-.78-.01-.055.487 0 0 .525.246.889 1.17 0 0 .463 1.428 2.688.944v1.489c0 .211-.129.459-.528.385-3.18-1.057-5.472-4.056-5.472-7.59 0-4.419 3.582-8 8-8s8 3.581 8 8c0 3.533-2.289 6.531-5.466 7.59z"/></svg>
     </span>
     </div> 
-    <div className="w-56 h-56 overflow-hidden rounded-[50%] flex justify-self-start">
+    <div className="flex justify-self-start overflow-hidden">
+    <div className="h-56 overflow-hidden rounded-full"> 
     <img src={photo} className="w-full h-full"></img>
+    </div>
     </div>
     </div>
     </div> 
