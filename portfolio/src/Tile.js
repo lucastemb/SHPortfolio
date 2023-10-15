993782819c1caf38425edafb53dfa1f81663ba20
Lucas Tembras <51347268+lucastemb@users.noreply.github.com>
2023-10-15 01:26:06 -0400

    mobile friendly and fully responsive website

--- portfolio/src/Tile.js
+++ portfolio/src/Tile.js
@@ -4,13 +4,16 @@ import './styles/App.css';
 function Tile(props) {
   return (
     <div className="bg-white rounded-lg mt-4 mb-4">
-      <div className="flex flex-row justify-around h-full">
-        <div className="flex flex-row w-[25%] p-4">
+      <div className="flex lg:flex-row md:flex-row flex-col justify-around h-full">
+        <div className="flex flex-row lg:w-[25%] md:w-[25%] p-4">
         <div className="flex justify-center items-center w-full">  
         <img className="max-h-80 place-self-center"src={props.img}></img>
-        </div> 
         </div>
-        <span className="flex flex-col w-[50%] p-8"> 
+        </div>
+        <div className="flex justify-center text-center lg:text-right md:text-right lg:w-[25%] md:w-[25%] align-right p-8 lg:hidden md:hidden">
+        <p className="text-[#001F5B] text-sm lg:text-base md:text-base"> {props.dates}</p>
+        </div> 
+        <span className="flex flex-col lg:w-[50%] md:w-[50%] p-8"> 
           <h1 className="font-bold text-sm lg:text-xl text-left text-[#001F5B]"> {props.company} <span className="font-semibold lg:text-xl"> - {props.role} </span> </h1>
           <ul className="list-disc text-[#001F5B] text-xs lg:text-sm max-h-full ml-4 p-4">
             {props.desc && props.desc.map(point => {
@@ -18,7 +21,7 @@ function Tile(props) {
             })}
           </ul>
         </span>
-        <div className="flex justify-center text-right w-[25%] align-right p-8">
+        <div className="flex justify-center text-right w-[25%] align-right p-8 hidden lg:flex md:flex">
         <p className="text-[#001F5B]"> {props.dates}</p>
         </div>
       </div>
